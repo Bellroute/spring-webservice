ff6680f5cbb4d9fab954df77745b59b3a0198331
Bellroute <kjkun7631@naver.com>
2018-12-26 01:22:54 +0900

    SpringBoot&JPA로 간단 API만들기

---
+++ src/test/java/com/bellroute/webservice/domain/posts/PostsRepositoryTest.java
@@ -0,0 +1,70 @@
+package com.bellroute.webservice.domain.posts;
+
+
+import org.junit.After;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PostsRepositoryTest {
+
+    @Autowired
+    PostsRepository postsRepository;
+
+    @After
+    public void cleanup() {
+        /**
+         이후 테스트 코드에 영향을 끼치지 않기 위해
+         테스트 메소드가 끝날때 마다 repository 전체를 비우는 코드
+         **/
+        postsRepository.deleteAll();
+    }
+
+    @Test
+    public void 게시글저장_불러오기() {
+        //given (테스트 기반 환경 구축)
+        postsRepository.save(Posts.builder()
+                .title("테스트 게시글")
+                .content("테스트 본문")
+                .author("kjkun7631@gmail.com")
+                .build());
+
+        //when (테스트 하고자 하는 행위 선언)
+        List<Posts> postsList = postsRepository.findAll();
+
+        //then (테스트 결과 검증)
+        Posts posts = postsList.get(0);
+        assertThat(posts.getTitle(), is("테스트 게시글"));
+        assertThat(posts.getContent(), is("테스트 본문"));
+    }
+
+    @Test
+    public void BaseTimeEntity_등록() {
+        //given
+        LocalDateTime now = LocalDateTime.now();
+        postsRepository.save(Posts.builder()
+                .title("테스트 게시글")
+                .content("테스트 본문")
+                .author("kjkun7631@gmail.com")
+                .build());
+
+        //when
+        List<Posts> postsList = postsRepository.findAll();
+
+        //then
+        Posts posts = postsList.get(0);
+        assertTrue(posts.getCreatedDate().isAfter(now));
+        assertTrue(posts.getModifiedDate().isAfter(now));
+    }
+}
