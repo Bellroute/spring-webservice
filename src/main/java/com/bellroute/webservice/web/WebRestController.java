ce0810d1b75d7ad6b1cc12506e5ccab786ae1c35
Bellroute <kjkun7631@naver.com>
2018-12-26 13:45:46 +0900

    refactor: add annotation '@AllArgsConstructor' in WebRestController class

--- src/main/java/com/bellroute/webservice/web/WebRestController.java
+++ src/main/java/com/bellroute/webservice/web/WebRestController.java
@@ -2,12 +2,14 @@ package com.bellroute.webservice.web;
 
 import com.bellroute.webservice.domain.posts.PostsRepository;
 import com.bellroute.webservice.dto.posts.PostsSaveRequestDto;
+import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@AllArgsConstructor
 public class WebRestController {
 
     private PostsRepository postsRepository;
